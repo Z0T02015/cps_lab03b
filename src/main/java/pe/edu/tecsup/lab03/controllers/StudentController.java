2884b270ff6969087d9c235312872364cba40a81
Cristhian Soto <cristhian.soto@Tecsup.edu.pe>
2025-09-05 18:00:03 -0500

    Probando cambios en StudentController en sprint-2

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,4 +1,9 @@
 package pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
+
+    public StudentController() {
+        System.out.println("Probando cambios en StudentController");
+    }
+
 }
